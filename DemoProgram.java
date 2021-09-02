a515bf004f4b12db81e4b5bb894a82d927213b5b
sarveshbondge <90001024+sarveshbondge@users.noreply.github.com>
2021-09-03 00:01:00 +0530

    removed sysout

--- DemoProgram.java
+++ DemoProgram.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class DemoProgram{
    public static void main(String args[]){
-     System.out.print("Enter two numbers : ");
+   
      Scanner sc = new Scanner(System.in);
      int a = sc.nextInt();
      int b = sc.nextInt();
